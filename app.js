c86fa86e7bc5bfe0538d76871ac02763fd546b28
ryan <rbach86@gmail.com>
2018-08-26 18:28:56 -0700

    Worked in starter js file instead of project directory updated project directory

--- app.js
+++ app.js
@@ -13,92 +13,55 @@ var scores, roundScore, activePlayer;
 
 scores = [0,0]; //scores read out of this array
 roundScore = 0;
-activePlayer = 0;
+activePlayer = 1;
+
 
 document.querySelector('.dice').style.display = 'none';
 
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
-                                                                // btn callback function 
+
+
+//
+function btn() {
+  //Do something here
+
+}
+btn();
+
+
+
+//Selecting button roll            two arguments,type of event & function called
 document.querySelector('.btn-roll').addEventListener('click', function () {
-    // 1. Random number needed.
-    var dice = Math.floor(Math.random() * 6) + 1; 
+//anomnymous function only used once
+  // 1. random number
+   var dice = Math.floor(Math.random()*6) +1;
+  // 2. display the result
+   var diceDom = document.querySelector('.dice');
+   diceDom.style.display = 'block';
+   diceDom.src = 'dice-' + dice + '.png';
 
-    //2. Display the result 
-    var diceDOM = document.querySelector('.dice') 
-    diceDOM.style.display = 'block';
-    diceDOM.src = 'dice-' + dice + '.png'
+  // 3. Update the round score IF the rolled number was not a one
 
-
-    //3. Update the round score IF the rolled number NOT a 1
-    if (dice !== 1) {
-        //Add score
-        roundScore += dice;
-        document.querySelector('#current-' + activePlayer).textContent = roundScore;
-    } else {
-        //Next player 
-       nextPlayer();
-        }
-
-    }); 
-
-    document.querySelector('btn-hold').addEventListener('click', function () {
-        //Add Current score to global score 
-        scores[activePlayer] += roundScore;
-        //Update the UI 
-        document.querySelector('#score' + activePlayer).textContent = scores[activePlayer];
-        // Check if player won the game (100 or over wins!)
-       if (scores[activePlayer] >= 100) {
-            document.querySelector('#name-'+ activePlayer).textContent = 'Winner!'; 
-            document.querySelector('.dice').style.display = 'none';
-            document.querySelector('.panel-' + activePlayer + '-panel').classList.add('winner');
-            document.querySelector('.panel-' + activePlayer + '-panel').classList.remove('active');
-       } else {
-                //Next player 
-                 nextPlayer();
-
-       }
-
-       // document.querySelector('.player-0-panel').classList.remove('active');
-       // document.querySelector('player-1-panel').classList.add('')
-        // if activeplayer = 0 then activeplayer 1 else active player = 0 
-
-       nextPlayer();
-    });
-
-    fucntion nextPlayer() {
-        //Next player
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; 
-        roundScore = 0; 
-        document.getElementById('current-0').textContent = '0';
-        document.getElementById('current-1').textContent = '0';
-
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
-
-       // document.querySelector('.player-0-panel').classList.remove('active');
-       // document.querySelector('player-1-panel').classList.add('')
-        // if activeplayer = 0 then activeplayer 1 else active player = 0 
-
-        document.querySelector('.dice').style.display = 'none';
-    };
+});
 
 
 
+// math.random gives random number and floor removes decimal
+// dice = Math.floor(Math.random()*6) +1;
+// This is a setter
+// document.querySelector('#current-' + activePlayer).textContent = dice;
+// must write HTML in string so JS parser doesn't error out
+// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 
+// This is a getter
+// var x = document.querySelector('#score-0').textContent;
+// console.log(x);
 
-
-
-
+// This selects the dice class in css and changes display to none
 
 
     // document.querySelector('#current-0').textContent// this doesnt work because textContent cannot interpret html*  = dice; //this renders the score dom manipulation
-//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice +'<em>' //This 
-//this is a setter 
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice +'<em>' //This
+//this is a setter
 
 //var x = document.querySelector('#score-0').textContent;// this is a getter
 //console.log(x);
-
-
